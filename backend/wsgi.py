701cb019da56c19ab792e927124c9b6936e66097
semper44 <emmanuelcopeters@gmail.com>
2024-02-07 12:01:09 +0000

    no module named django

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -7,7 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
+
 import os
+import django
+django.setup()
 
 from django.core.wsgi import get_wsgi_application
 
